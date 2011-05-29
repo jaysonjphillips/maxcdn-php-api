c0c944b5480fee71beb462528af948f323ecefdd
Jayson J Phillips <jayson.phillips@chroniumlabs.com>
2011-05-29 13:14:40 -0400

    regenerated docs. added a few more methods from the report namespace. dropped in some more tests. updated .gitignore to ignore my test_settings.php file

--- tests/Reports.php
+++ tests/Reports.php
@@ -7,24 +7,30 @@ class Reports extends PHPUnit_Framework_TestCase {
 	
 	function __construct() {
 		$this->maxcdn = null;
+		$this->result = null;
+		$this->company_id = null;
 	}
 	
 	protected function setUp() {
 		$this->maxcdn = new MaxCDN(APIKEY, USERID);
 		$this->test_date = date('c');
 	}
-	/**
-     * @expectedException MissingParameterException
-     */
-	public function testGetTotalTransferRequiredParamsNotNull() {
-		$result = $this->maxcdn->getTotalTransfer(null, null);
-	}
 	
 	/**
-     * @expectedException MissingParameterException
+     * 
      */
-	public function testGetTotalTransferRequiredParamsNotEmpty() {
-		$result = $this->maxcdn->getTotalTransfer('', '');
+	public function testGetTotalTransfer() {
+		$result = $this->maxcdn->getTotalTransfer('1212', 1);
+		$this->assertInternalType('integer', $result->faultCode());
+		
+		$this->setExpectedException('MissingRequirementException');
+		$this->result = $this->maxcdn->getTotalTransfer(null, null);
+		
+		$this->setExpectedException('MissingRequirementException');
+		$this->result = $this->maxcdn->getTotalTransfer('', '');
+		
+
+		
 	}
 	
 }
