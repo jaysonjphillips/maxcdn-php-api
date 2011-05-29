c4af9bc2e2cb4fe543d18922f585f330f4cf4d8d
Jayson J Phillips <jayson.phillips@chroniumlabs.com>
2011-05-29 13:34:04 -0400

    commented Test_Settings.php

--- tests/Test_Settings.php
+++ tests/Test_Settings.php
@@ -1,7 +1,13 @@
 <?php
 
 date_default_timezone_set('America/Los_Angeles');
+// Add your API key Here for testing
 define("APIKEY", null);
-define("USERID", null);
+
+// Add your user id here for testing
+define("USERID", null;
+
+// test value for current bandwidth. Will change to a fixture or something at some point
+define("CURRENT_BANDWIDTH", 1000000000000);
 
 ?>
